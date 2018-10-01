d475e9a258939f1e0b20a7157083f18e9fc34e7a
nalbam <me@nalbam.com>
2018-10-01 19:22:34 +0900

    dynamodb

--- main.tf
+++ main.tf
@@ -38,7 +38,7 @@ module "dev-sms-slack" {
   certificate_arn = "${module.domain.certificate_arn}"
   domain_name     = "${var.stage}-${var.name}.${var.domain}"
 
-  dynamodb = "true"
+  dynamodb = true
 
   env_vars = {
     PROFILE        = "${var.stage}"
