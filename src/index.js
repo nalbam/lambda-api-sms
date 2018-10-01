ea147ff20ca7c03c13f72d94d6961d7dfbbb67b2
nalbam <me@nalbam.com>
2018-10-01 20:35:18 +0900

    list

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const create = require('./sms/create').create;
+const list = require('./sms/list').list;
 
 exports.handler = (event, context, callback) => {
     console.log('## handler event : ', JSON.stringify(event, null, 2));
@@ -10,6 +11,9 @@ exports.handler = (event, context, callback) => {
     switch (httpMethod) {
     case 'POST':
         create(event, context, callback);
+        break;
+    case 'GET':
+        list(event, context, callback);
         break;
     default:
         callback(new Error(`Unrecognized operation "${httpMethod}"`));
