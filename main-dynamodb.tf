3c0b66f44a8980de0aa0e6ea652e96041ce11802
nalbam <me@nalbam.com>
2018-10-02 20:27:25 +0900

    rm line

--- main-dynamodb.tf
+++ main-dynamodb.tf
@@ -17,21 +17,6 @@ resource "aws_dynamodb_table" "dynamodb" {
     type = "S"
   }
 
-#   ttl {
-#     attribute_name = "TimeToExist"
-#     enabled = false
-#   }
-
-#   global_secondary_index {
-#     name               = "GameTitleIndex"
-#     hash_key           = "GameTitle"
-#     range_key          = "TopScore"
-#     write_capacity     = 10
-#     read_capacity      = 10
-#     projection_type    = "INCLUDE"
-#     non_key_attributes = ["UserId"]
-#   }
-
   tags {
     Name = "${var.stage}-${var.name}"
   }
